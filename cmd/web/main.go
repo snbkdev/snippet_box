ed6cfec36e45f35737466b91300151fbc69ffb1b
Asanbek09 <samudinua@gmail.com>
2025-10-16 10:40:16 +0600

    Добавлены другие пути

---
+++ cmd/web/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Snippet-Box"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet ..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet ..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("starting server on : 4060")
+
+	err := http.ListenAndServe(":4060", mux)
+	log.Fatal(err)
+}
