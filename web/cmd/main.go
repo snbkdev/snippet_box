1610df9de31d4fc229ea48983f9101323d646df6
Asanbek09 <samudinua@gmail.com>
2025-10-16 10:28:10 +0600

    Небольшие изменения

--- web/cmd/main.go
+++ web/cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Snippet-Box"))
+}
 
 func main() {
-	fmt.Println("Welcome to the Snippet-Box!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on : 4060")
+
+	err := http.ListenAndServe(":4060", mux)
+	log.Fatal(err)
 }
