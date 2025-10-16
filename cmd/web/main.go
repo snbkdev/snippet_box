3666980c2e133eafd25c5d2fec7ff2a34468a1cb
Asanbek09 <samudinua@gmail.com>
2025-10-16 14:24:03 +0600

    Изменение структуры проекта

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "public, max-age=31536000")
-	
-	w.Header().Add("Cache-Control", "public")
-	w.Header().Add("Cache-Control", "max-age=31536000")
-
-	w.Header().Del("Cache-Control")
-
-	w.Header().Get("Cache-Control")
-
-	w.Header().Values("Cache-Control")
-
-	w.Write([]byte("Welcome to Snippet-Box"))
-}
-
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
-	//io.WriteString(w, msg)
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet ..."))
-}
-
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Use the w.WriteHeader() method to send a 201 status code
-	w.WriteHeader(http.StatusCreated)
-
-	w.Write([]byte("Save a new snippet ..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
