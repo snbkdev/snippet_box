80a1c93f6a52db0b69a0c734427156362e131e51
Asanbek09 <samudinua@gmail.com>
2025-10-24 11:36:59 +0600

    snippets.go изменения в методе Latest

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -39,13 +39,9 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	stmt := `select id, title, content, created, expires from snippets where expires > CURRENT_TIMESTAMP AT TIME ZONE 'UTC' and id = $1 `
-
-	row := m.DB.QueryRow(stmt, id)
-
 	var s Snippet
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := m.DB.QueryRow("select id, title, content, created, expires from snippets where expires > CURRENT_TIMESTAMP AT TIME ZONE 'UTC' and id = $1", id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return Snippet{}, ErrNoRecord
@@ -58,5 +54,30 @@ func (m *SnippetModel) Get(id int) (Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	stmt := `select id, title, content, created, expires from snippets where expires > CURRENT_TIMESTAMP AT TIME ZONE 'UTC' order by id desc limit 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []Snippet
+
+	for rows.Next() {
+		var s Snippet
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
