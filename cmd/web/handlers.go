375a0ec843a6f9e078ee73889b32cdc861245255
Asanbek09 <samudinua@gmail.com>
2025-10-24 11:13:45 +0600

    handlers.go изменения в методе snippetView

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
+	"lets_go/internal/models"
 	"net/http"
 	"strconv"
 )
@@ -35,7 +37,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d ...", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
