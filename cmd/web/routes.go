a4401d6980f8576076393328f244b4a1b4688a6f
Asanbek09 <samudinua@gmail.com>
2025-10-31 16:47:22 +0600

    Небольшая защита

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,16 +16,18 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-	// Для пользователя 
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignUp))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignUpPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogIn))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLogInPost))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogOutPost))
+
+	protected := dynamic.Append(app.requireAuthentication)
+	
+	// Защищенный
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogOutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
-
 	return standard.Then(mux)
 }
