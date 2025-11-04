feadbaf0c8faf84b9903a94678848d15785608ed
Asanbek09 <samudinua@gmail.com>
2025-11-04 13:47:25 +0600

    Тестирование signup метода

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 
@@ -87,3 +88,21 @@ func extractCSRFToken(t *testing.T, body string) string {
 
 	return html.UnescapeString(matches[1])
 }
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
